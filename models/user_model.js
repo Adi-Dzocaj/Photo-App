41dcbd6bf8c7b8c53f86c65f6347033d1b106177
Adi Dzocaj <adidzocaj@gmail.com>
2022-03-09 02:34:47 +0100

    Fixed minor bug

--- models/user_model.js
+++ models/user_model.js
@@ -1,38 +1,39 @@
+
 const bcrypt = require('bcrypt');
 
 module.exports = (bookshelf) => {
-    return bookshelf.model('User', {
-            tableName: 'users',
-            photos() {
-                return this.hasMany('Photo');
-            },
-            albums() {
-                return this.hasMany('Album');
-            }
-        },
-		{
+	return bookshelf.model('User', {
+		tableName: 'users',
+		photos() {
+			return this.hasMany('Photo');
+		},
+    albums() {
+		return this.hasMany('Album');
+    }
+	}, {
 		async login(email, password) {
-			// find user based on the username (bail if no such user exists)
+	
+			// find user based on the email ( bail if no such email exists )
 			const user = await new this({ email }).fetch({ require: false });
 			if (!user) {
 				return false;
 			}
+			
 			const hash = user.get('password');
-
+	
 			// hash the incoming cleartext password using the salt from the db
 			// and compare if the generated hash matches the db-hash
 			const result = await bcrypt.compare(password, hash);
 			if (!result) {
 				return false;
 			}
-
-			// return user
+	
+			// return the user
 			return user;
-		}},
-		{
-            async fetchById(id, fetchOptions = {}) {
-                return await new this({ id }).fetch(fetchOptions);
-            }
-        },
-    );
+		},
+
+		async fetchById(id, fetchOptions = {}) {
+			return await new this({ id }).fetch(fetchOptions);
+		}
+	});
 };
